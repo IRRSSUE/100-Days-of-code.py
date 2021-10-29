a0b335c3550e53190aa1606adf9ac6fce66305a7
Austin Rolert <austinrolert@gmail.com>
2021-10-29 09:14:45 +0630

    Create Day-8.py
    below is basic function - 
    
    def greeting():
    
           print("hello")
    
           print("hey.. how yu doing these days")
    
          
    
    everytime you call this function greetin() this will print out these two lines of code but think about it what if you can add the names of the person you greeting... isn't nice ? 
    
    
    
    to do this add a main variable in def name(var):
    
    
    
    to use the variable name you can just use it like a simple variable 
    
    for example 
    
    
    
    basic code : 
    
    var = input("the person name you're gonna greet with")
    
    print(f"Hello {var}") 
    
    
    
    the def code :
    
    
    
    def greeting_to(name):
    
           print(f"Hello {name}")           or you can use print("hello", name)
    
    
    
    greeting_to("Shit xD")
    
    
    
    this out put of both code : Hello name 
    
    this way is also call input in function
    
    
    
    Parameter vs Argument :
    
    parameter is the variable name or input in function like this - def name(name11):      -- in this name11 is parameter 
    
    
    
    and 
    
    
    
    Argumet is a data that will go into parameter ( it's has been use when the function is called 
    
    like this - name("irrssue") -- in this "irrssue" is parameter )   the reson I added "" is to make sure this is sting xD ( if you don't want to add "" you can what ever you like )
    
    butt the question is what if there is two input ? 
    
    then just use two variable :) 
    
    
    
    def person(name, age) -- like this 
    
    when it's use: 
    
    person("irrssue", 16) -- that's all :)) 
    
    
    
    .index 
    
    that is the keywords that only works on list 
    
    it's show the letter that you want to know it's place in indext 
    
    example 
    
    list = ["ikd", "wtf", "help", "fuk"]
    
    list.index("wtf")
    
    
    
    if you wanna print just use it in variable :
    
    
    
    idk12 = list.index("wtf")
    
    print(ikd12)
    
    
    
    output: 1 
    
    
    
    note: if there is 2 wtf in the list the index is only choose the first wtf in the list : ) 

---
+++ Day-8.py
@@ -0,0 +1,49 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' , 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
+
+def caesar(text_amount, shift_amount, direction_on):
+  if direction_on == "encode":
+    plain_text = ""
+    for letter in text_amount:
+      position = alphabet.index(letter)
+      new_position = position + shift_amount
+      plain_text += alphabet[new_position]
+    print(f"THe encode text is {plain_text}")
+
+  elif direction_on == "decode":
+    cipher_text = ""
+    for letter2 in text_amount:
+      position2 = alphabet.index(letter2)
+      new_position2 = position2 - shift_amount
+      cipher_text += alphabet[new_position2]
+    print(f"THe decode text is {cipher_text}")
+
+
+caesar(text_amount=text, shift_amount=shift, direction_on=direction)
+# def encrypt(plain_text, shift_amount):
+#   cipher_text = ""
+#   for letter in plain_text:
+#     position = alphabet.index(letter)
+#     new_position = position + shift_amount
+#     cipher_text += alphabet[new_position]
+#   print(f"The encoded text is {cipher_text}")
+
+# def decrypt(cipher_text, shift_amount):
+#   plain_text = ""
+#   for letter in cipher_text:
+#     position = alphabet.index(letter)
+#     new_position = position - shift_amount
+#     plain_text += alphabet[new_position]
+#   print(f"The decoded text is {plain_text}")
+
+# if direction == "encode":
+#   encrypt(plain_text=text, shift_amount=shift)
+# elif direction == "decode":
+#   decrypt(cipher_text=text, shift_amount=shift)
+
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
